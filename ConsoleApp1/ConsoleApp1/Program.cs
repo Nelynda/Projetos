8cfbaa030f259bb6832b88ed76826b65d7bfe432
Jose.lsantos <jose.lsantos@al.infnet.edu.br>
2021-03-16 22:32:20 -0300

    PROVOCAR CONFLITOS

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,40 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Escreva seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            Console.WriteLine("Escreva sua idade: ");
+            int idade  = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Escreva seu CEP: ");
+            string CEP = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu endereco: ");
+            string endereco = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu bairro: ");
+            string bairro = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu completo: ");
+            string complemento = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu numero: ");
+            int numero = Convert.ToInt16(Console.ReadLine());
+
+
+
+
+            Console.WriteLine(nome + " " + sobrenome);
+
+            Console.WriteLine("Aperte o enter para finalizar...");
+            Console.ReadLine();
+            Console.WriteLine("Finalizou!");
         }
     }
 }
